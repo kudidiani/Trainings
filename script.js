020060e2ce7181a11f0ff25330d0b7103287b117
Diana <ku.didiani@gmail.com>
2022-08-24 14:03:21 +0300

    concatenation and Back quotes

--- script.js
+++ script.js
@@ -19,27 +19,39 @@
 // const type = typeof(x); // ... typeof x; without()
 // console.log(type);
 
-let num1 = 10 ** 2; // 10 to the power of 2
-console.log(num1);
-let num2 = 10 % 3; // num2 = 1 -> 10/3 = 3 and 1 rest
-console.log('The rest of the 10/3 operation is:', num2);
-/*if we want to find out the last digit in a number,
- we calculate the remainder of dividing by 10 */
- let x;
- x = 249;
- let num3 = x % 10;
- console.log('The last digital in number', x, ' is:', num3);
- /*if we want to find out if the number is odd, or even, we should calculate 
- the rest of the division of the number on 2. If the rest is 0 the number is even, 
- visa verse if the rest is 1 this is an odd number*/
- let y;
- y = 655;
- let num4 = y % 2;
- console.log(num4);
+// let num1 = 10 ** 2; // 10 to the power of 2
+// console.log(num1);
+// let num2 = 10 % 3; // num2 = 1 -> 10/3 = 3 and 1 rest
+// console.log('The rest of the 10/3 operation is:', num2);
+// /*if we want to find out the last digit in a number,
+//  we calculate the remainder of dividing by 10 */
+//  let x;
+//  x = 249;
+//  let num3 = x % 10;
+//  console.log('The last digital in number', x, ' is:', num3);
+//  /*if we want to find out if the number is odd, or even, we should calculate 
+//  the rest of the division of the number on 2. If the rest is 0 the number is even, 
+//  visa verse if the rest is 1 this is an odd number*/
+//  let y;
+//  y = 655;
+//  let num4 = y % 2;
+//  console.log(num4);
 
- let figure = 'rectangle';
- console.log('calculate the area of a', figure);
- let a = 5;
- let b = 9;
- let rectangleArea = a*b;
- console.log('The area of the',figure, 'is:',rectangleArea);
+//  let figure = 'rectangle';
+//  console.log('calculate the area of a', figure);
+//  let a = 5;
+//  let b = 9;
+//  let rectangleArea = a*b;
+//  console.log('The area of the',figure, 'is:',rectangleArea);
+
+const myName = 'Diana';
+let myAge = 19;
+myAge = myAge + 1;//21
+let message = myName + ' is ' + myAge; // concatenation
+
+let message2 = `${myName} 
+is ${myAge + 3}`;  /*Diana
+                     is 23 */
+//  Back quotes for  put `${} ... ` number in string and saves formatting 
+console.log(message);
+console.log(message2);
