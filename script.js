227fb6423b55ea7798cfff328109545d8ead80df
Diana <ku.didiani@gmail.com>
2022-08-24 13:03:50 +0300

    typeof

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-let amount = 25; // I have 25 UAH
-let cookiePrice = 7; 
-let candyPrice = 8;
-// Can i buy both: cookie and candy?
-let rest = amount - cookiePrice; // the rest after buying cookie
-console.log('Rest is: ', rest);
+// let amount = 25; // I have 25 UAH
+// let cookiePrice = 7; 
+// let candyPrice = 8;
+// // Can i buy both: cookie and candy?
+// let rest = amount - cookiePrice; // the rest after buying cookie
+// console.log('Rest is: ', rest);
 
-const hasEnoughMoney = rest >= candyPrice; // const hasEnoughMoney = true/false
-console.log('has enough money for candy: ', hasEnoughMoney);
+// const hasEnoughMoney = rest >= candyPrice; // const hasEnoughMoney = true/false
+// console.log('has enough money for candy: ', hasEnoughMoney);
 
-if (hasEnoughMoney) {
-    console.log('I have enough money')
-} else{
-    console.log('I do not have enough money')
-}
+// if (hasEnoughMoney) {
+//     console.log('I have enough money')
+// } else{
+//     console.log('I do not have enough money')
+// }
+
+let x = true;
+// console.log(typeof(x));
+const type = typeof(x); // ... typeof x; without()
+console.log(type);
