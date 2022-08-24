1bd81aec4d5b30414c875f31d4e55aa073e2fd5f
Diana <ku.didiani@gmail.com>
2022-08-24 12:54:28 +0300

    add boolean

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-let amount = 18; // I have 10 UAH
+let amount = 25; // I have 25 UAH
 let cookiePrice = 7; 
 let candyPrice = 8;
 // Can i buy both: cookie and candy?
 let rest = amount - cookiePrice; // the rest after buying cookie
-if (rest >= candyPrice) {
-    document.write('I have enough money')
+console.log('Rest is: ', rest);
+
+const hasEnoughMoney = rest >= candyPrice; // const hasEnoughMoney = true/false
+console.log('has enough money for candy: ', hasEnoughMoney);
+
+if (hasEnoughMoney) {
+    console.log('I have enough money')
 } else{
-    document.write('I do not have enough money')
+    console.log('I do not have enough money')
 }
